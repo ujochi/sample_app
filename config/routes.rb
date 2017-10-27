5ec4a525a3c0503a7b13f4c1c0847f19c40e269a
ujochi <evo670@wp.pl>
2017-10-27 20:46:51 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   get  '/singup', to: 'users#new'
+  post '/signup',  to: 'users#create'
   get  '/home', to: 'static_pages#home'
   get  '/about', to: 'static_pages#about'
   get  '/help', to: 'static_pages#help'
