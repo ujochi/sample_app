e0bce172cd31fdfebc0068d11fa91bd16cca09bd
ujochi <evo670@wp.pl>
2017-10-27 22:00:28 +0200

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+
   resources :users
   get  '/singup', to: 'users#new'
   post '/signup',  to: 'users#create'
@@ -7,4 +9,8 @@ Rails.application.routes.draw do
   get  '/help', to: 'static_pages#help'
   get  '/contact', to: 'static_pages#contact'
   root 'static_pages#home'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 end
