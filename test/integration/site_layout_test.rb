a2c645b6d69420ece612cd7d220f521270c51c8f
ujochi <evo670@wp.pl>
2017-10-28 17:54:21 +0200

    befor delete users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,6 +14,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
   end
