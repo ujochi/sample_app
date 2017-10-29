96891ce514562d46bf9f1e5889a3b63b6de01847
ujochi <evo670@wp.pl>
2017-10-29 15:26:40 +0100

    test email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,15 +90,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  host = 'nameless-depths-40390.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, port: 33544 }
   ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
+      :address        => 'smtp.gmail.com',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => 'cinema.managment.system@gmail.com',
+      :password       => 'Irmina6701',
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
+  config.action_mailer.perform_deliveries = true
 end
